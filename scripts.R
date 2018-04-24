b4f4e8217ff4124a623d60d7c07d5324abc5710d
Pham <ddpham@ads.iu.edu>
2018-04-24 13:08:32 -0400

    finish draft of EM algorithm. added Kushal's generate_mog function, with a modification to allow unequal prior probabilities.

--- scripts.R
+++ scripts.R
@@ -4,7 +4,25 @@ if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only = TRUE)
 rm(list.of.packages, new.packages)
 
-initial_setup = function(n_classes, prior_choice='equal_priors_unif_params', data){
+# Generates MOG data given # of observations, means and standard deviatins of each Normal Distribution
+
+# n_data: integer number of data points to generate
+# means: vector of means for each class
+# sds: vector of standard deviations of each class
+# priors: vector of prior probabilities for each class
+
+# e.g.: MOG for 100 observations from N(0,1) and 500 observations from N(10,2) with equal priors would be:
+# generate_mog(c(100,500),c(0,10),c(1,2))
+
+generate_mog <- function(n_data,means,sds,priors=NULL) {
+  n_classes = length(means)
+  if(is.null(priors)){ priors = rep(1/n_classes, n_classes) }
+  classes = sample(1:n_classes, size=n_data, replace=TRUE, prob=priors)
+  data = rnorm(n_data, mean = means[classes], sd = sds[classes])
+  return (data)
+}
+
+EM_setup = function(n_classes, prior_choice='equal_priors_unif_params', data){
 	n_vars = ncol(data)
 	if(prior_choice=='equal_priors_random_params'){
 		prior = matrix(rep(1/n_classes, n_classes), nrow=n_classes) #All classes are equally as likely.
@@ -13,7 +31,7 @@ initial_setup = function(n_classes, prior_choice='equal_priors_unif_params', dat
 	} else {
 		stop('invalid prior choice.')
 	}
-	return(list(prior, params)))
+	return(list(prior, mu, sigma))
 }
 
 expectation = function(data, prior, mu, sigma){
@@ -30,43 +48,26 @@ expectation = function(data, prior, mu, sigma){
 }
 
 maximization = function(P.Ci.given.Xj, data, prior){
+  n_classes = ncol(prior)
+  n_vars = ncol(data)
+  n_data = nrow(data)
 	N = matrix(apply(P.Ci.given.Xj, 1, sum), ncol=1)
 	mu = matrix(rep(NA, length=n_classes*n_vars, 0, 1), nrow=n_classes)
+	sigma = replicate(n_classes, matrix(rep(NA, n_vars^2), nrow=n_vars))
 	for(i in 1:n_classes){
 		mu[i,] = colMeans(data * P.Ci.given.Xj[i] / N[i])
-		sigma[,,i] = 
+		diff = data - mu[rep(i, nrow(data)),]
+		sigma[,,i] = diff %&% t(diff) / n_data
 	}
 	prior = N / sum(N)
-	mu = P.Ci.given.Xj * data
-
 }
-        sigma <- (Y %*% t(Y) + diag(extra_var)) / n
-
-function [new_params,new_priors] = maxim(full_vector,data,priors)
-    new_params = []
-    new_priors = []
-    for i = 1:length(priors)
-        total=0
-        total_squares=0
-        points_distribution = sum(full_vector)
-        points = points_distribution(i)
-        for j = 1:length(data)
-            total = total + full_vector(j,i)*data(j)
-            total_squares = total_squares + full_vector(j,i)*data(j)^2
-        end
-        mean = total/points
-        sd = sqrt((total_squares/points)-(mean^2))
-        prior = points/sum(points_distribution)
-        new_params = [new_params,mean,sd]
-        new_priors = [new_priors,prior]
 
 
+#prior_and_params = EM_setup(3)
+#full_vector = expec(dat,prior,params)
+#[params,prior] = maxim(full_vector,dat,prior)
 
-prior_and_params = initial_setup(3)
-full_vector = expec(dat,prior,params)
-[params,prior] = maxim(full_vector,dat,prior)
-
-for i = 1:15
-    full_vector = expec(dat,prior,params)
-    [params,prior] = maxim(full_vector,dat,prior)
-end
+#for i = 1:15
+#    full_vector = expec(dat,prior,params)
+#    [params,prior] = maxim(full_vector,dat,prior)
+#end
