4cfc515d0174585f7443c95cf3460ecb15c75461
Pham <ddpham@ads.iu.edu>
2018-04-24 15:46:31 -0400

    beginning to debug

--- scripts.R
+++ scripts.R
@@ -1,4 +1,4 @@
-list.of.packages = c('knitr', 'mvtnorm')
+list.of.packages = c('knitr', 'mvtnorm','MASS')
 new.packages = list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only = TRUE)
@@ -8,29 +8,33 @@ rm(list.of.packages, new.packages)
 
 # n.data: integer number of data points to generate
 # means: vector of means for each class
-# sds: vector of standard deviations of each class
+# sigma: covariance matrix between the classes
 # priors: vector of prior probabilities for each class
 
 # e.g.: MOG for 100 observations from N(0,1) and 500 observations from N(10,2) with equal priors would be:
-# generate_mog(c(100,500),c(0,10),c(1,2))
+# generate.mog(c(100,500),c(0,10),c(1,2))
 
-generate_mog <- function(n.data, means, sds, priors=NULL) {
-  n.classes = length(means)
-  if(is.null(priors)){ priors = rep(1/n.classes, n.classes) }
-  if((length(means) != length(sds)) | (length(sds) != length(priors))){
-    stop('Each class must have a mean, sd, and prior.')
-  }
+generate.mog <- function(n.data, mu, sigma, priors=NULL) {
+  if(is.vector(data)){
+    data = as.matrix(data, ncol=1)
+    mu = as.matrix(mu, ncol=1)
+    new.sigma = array(0,c(1,1, nrow(mu)))
+    new.sigma[1,1,] = sigma
+    sigma = new.sigma
+  } 
+  n.classes = nrow(mu)
+  if(is.null(priors)){ priors = rep(1/n.classes, n.classes) } #Equal priors by default.
   classes = sample(1:n.classes, size=n.data, replace=TRUE, prob=priors)
-  data = rnorm(n.data, mean = means[classes], sd = sds[classes])
+  data = mvrnorm(n = 1, mu=mu[classes,], Sigma=sigma[,,classes])
   return (data)
 }
 
-EM_setup = function(data, n.classes, prior.choice='equal_priors_unif_params', ...){
-	n_vars = ncol(data)
-	if(prior.choice=='equal_priors_random_params'){
+EM.setup = function(data, n.classes, prior.choice='equal.priors.unif.params', ...){
+	n.vars = ncol(data)
+	if(prior.choice=='equal.priors.random.params'){
 		prior = matrix(rep(1/n.classes, n.classes), nrow=n.classes) #All classes are equally as likely.
-		mu = matrix(runif(n.classes*n_vars, 0, 1), nrow=n.classes) #All means are initially ~Unif[0,1].
-		sigma = replicate(n.classes, matrix(runif(n_vars^2, 0, 1), nrow=n_vars))
+		mu = matrix(runif(n.classes*n.vars, 0, 1), nrow=n.classes) #All means are initially ~Unif[0,1].
+		sigma = replicate(n.classes, matrix(runif(n.vars^2, 0, 1), nrow=n.vars))
 	} else if(prior.choice=='means'){
 	  #do something.
 	} else {
@@ -44,7 +48,7 @@ EM_setup = function(data, n.classes, prior.choice='equal_priors_unif_params', ..
 expectation = function(data, mu, sigma, prior){
 	#Get the probabilities of each data point belonging to each class.
 	n.classes = ncol(prior)
-	n_vars = ncol(data)
+	n.vars = ncol(data)
 	n.data = nrow(data)
 	P.Xj.and.Ci = matrix(rep(NA, length=n.classes*n.data), ncol=n.classes)
 	for(i in 1:n.classes){
@@ -61,11 +65,11 @@ expectation = function(data, mu, sigma, prior){
 
 maximization = function(P.Ci.given.Xj, data, prior){
   n.classes = ncol(prior)
-  n_vars = ncol(data)
+  n.vars = ncol(data)
   n.data = nrow(data)
 	N = matrix(apply(P.Ci.given.Xj, 1, sum), ncol=1)
-	mu = matrix(rep(NA, length=n.classes*n_vars, 0, 1), nrow=n.classes)
-	sigma = replicate(n.classes, matrix(rep(NA, n_vars^2), nrow=n_vars))
+	mu = matrix(rep(NA, length=n.classes*n.vars, 0, 1), nrow=n.classes)
+	sigma = replicate(n.classes, matrix(rep(NA, n.vars^2), nrow=n.vars))
 	for(i in 1:n.classes){
 		mu[i,] = colMeans(data * P.Ci.given.Xj[i] / N[i])
 		diff = data - mu[rep(i, nrow(data)),]
@@ -76,14 +80,14 @@ maximization = function(P.Ci.given.Xj, data, prior){
 	return(out)
 }
 
-EM(data, n.classes, prior.choice, tol=.001, max_iters=1000, ...){
-  init = EM_setup(data, n.classes, prior.choice, ...)
+EM = function(data, n.classes, prior.choice, tol=.001, max.iters=1000, ...){
+  init = EM.setup(data, n.classes, prior.choice, ...)
   mu = init$mu
   sigma = init$sigma
   prior = init$prior
   
-  log.like = rep(-Inf, max_iters)
-  for(i in 1:max_iters){
+  log.like = rep(-Inf, max.iters)
+  for(i in 1:max.iters){
     exp = expectation(data, mu, sigma, prior)
     P.Ci.given.Xj = exp$P.Ci.given.Xj
     log.like[i] = exp$log.like
@@ -95,5 +99,8 @@ EM(data, n.classes, prior.choice, tol=.001, max_iters=1000, ...){
     
     if(abs(log.like) < tol){ break }
   }
-  return(max)
+  log.like = log.like[1:i]
+  out = list(mu, sigma, prior, log.like)
+  names(out) = c('mu','sigma','prior', 'log.like')
+  return(out)
 }
