655afe85b2aada71b30a4787f52235362c23bf2a
damondpham <ddpham@iu.edu>
2018-04-28 14:10:13 -0400

    multivariate em algorithm can return a result now. it's not a good one though :)

--- scripts.R
+++ scripts.R
@@ -1,4 +1,4 @@
-list.of.packages = c('knitr', 'mvtnorm','MASS')
+list.of.packages = c('knitr', 'mixtools','MASS', 'Matrix', 'stats')
 new.packages = list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only = TRUE)
@@ -30,12 +30,19 @@ EM.setup = function(data, n.classes, prior.choice='equal.priors.unif.params', ..
     data = as.matrix(data, ncol=1)
   }
 	n.vars = ncol(data)
+	
 	if(prior.choice=='equal.priors.random.params'){
+	  generate.sigma = function(nrow=n.vars){
+	    A = matrix(runif(n.vars^2, 0, 1), nrow=n.vars)
+	    return(t(A) %*% A)
+	  }
 		prior = rep(1/n.classes, n.classes) #All classes are equally as likely.
 		mu = matrix(runif(n.classes*n.vars, 0, 1), nrow=n.classes) #All means are initially ~Unif[0,1].
-		sigma = replicate(n.classes, matrix(runif(n.vars^2, 0, 1), nrow=n.vars))
+		sigma = replicate(n.classes, generate.sigma(nrow=n.vars))
+		
 	} else if(prior.choice=='means'){
 	  #do something.
+	  
 	} else {
 		stop('Invalid prior choice.')
 	}
@@ -59,9 +66,11 @@ expectation = function(data, mu, sigma, prior){
 	} else {
   	for(i in 1:n.classes){
   	  #P(Xj.and.Ci) = P(Ci) * P(Xj | Ci)
-	    P.Xj.and.Ci[,i] = prior[i] * dmvnorm(data, mean=mu[i,], sigma=sigma[,,i])
+	    P.Xj.and.Ci[,i] = prior[i] * dmvnorm(data, mu=mu[i,], sigma=sigma[,,i])
 	  }
 	}
+	#Infinite values are returned at very low densities. Replace them with a low value.
+	P.Xj.and.Ci[which(is.infinite(P.Xj.and.Ci), arr.ind=TRUE)] = min(P.Xj.and.Ci) * 1e-10
 	P.Ci.given.Xj = P.Xj.and.Ci / apply(P.Xj.and.Ci, 1, sum)
 	N = apply(P.Ci.given.Xj, 2, sum)
 	log.like = -sum(log(apply(P.Xj.and.Ci, 1, sum)))
@@ -81,7 +90,8 @@ maximization = function(P.Ci.given.Xj, N, data, prior){
 	  if(n.vars==1){
 	    sigma[i] = sqrt(t(data^2) %*% P.Ci.given.Xj[,i] / N[i] - mu[i,]^2)
 	  } else {
-	    sigma[,,i] = sqrt(t(data^2) %*% P.Ci.given.Xj[,i] / N[i] - mu[i,]^2)
+	    diff = data - apply(data, 2, mean)
+	    sigma[,,i] = sqrt(t(diff) %*% (diff * P.Ci.given.Xj[,i]) / N[i])
 	  }
 	}
 	prior = N/sum(N)
@@ -90,7 +100,7 @@ maximization = function(P.Ci.given.Xj, N, data, prior){
 	return(out)
 }
 
-EM = function(data, n.classes, prior.choice, tol=1e-3, max.iters=500, ...){
+EM = function(data, n.classes, prior.choice, tol=1e-5, max.iters=500, ...){
   init = EM.setup(data, n.classes, prior.choice, ...)
   mu = init$mu
   sigma = init$sigma
